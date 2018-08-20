dcde814ca0e0c87556b612732e18e1c8fae7d695
Jeffrey van Raaij <36259720+jvanraaij@users.noreply.github.com>
2018-08-20 11:51:39 +0200

    Added HelloWorld example.

---
+++ HelloWorldProgram.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld
+{
+    class HelloWorldProgram
+    {
+        private const string VERSION = "0.1";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"HelloWorld version {VERSION}");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
